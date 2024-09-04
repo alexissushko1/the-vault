280ac181cd30ef5999800b697f377c69614178fd
Alexis Sushko <alexis.sushko@gmail.com>
2024-09-04 17:50:22 -0400

    Added step 3 and step 3a into pseudocode in index.js

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ Step 1: Create a string for user that says: You have received this message becau
 Step 1a: console.log(vaultMessage);
 Step 2: Assign a first variable containing the result of a calculation using an arithmetic operator to equal the first code in the combination.
 Step 2a: Calculation using arithmetic operator: 25 - 15
+Step 3: Assign a second variable containing the result of a calculation using an arithmetic operator to equal the second code in the combination.
+Step 3a: Calculation using arithmetic operator: 10 * 4
 */
