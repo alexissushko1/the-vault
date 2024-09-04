36be75e327eb64dd37199db302dee869d4e9d673
Alexis Sushko <alexis.sushko@gmail.com>
2024-09-04 17:53:17 -0400

    Added step 5 and step 5a in pseudocode in index.js

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ Step 2: Assign a first variable containing the result of a calculation using an 
 Step 2a: Calculation using arithmetic operator: 25 - 15
 Step 3: Assign a second variable containing the result of a calculation using an arithmetic operator to equal the second code in the combination.
 Step 3a: Calculation using arithmetic operator: 10 * 4
+Step 4: Assign the last variable containing the result of a calculation using an arithmetic operator to equal the last code in the combination.
+Step 4a: Calculation using arithmetic operator: 50 - 11
 */
